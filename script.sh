d825a8f90c3bfbff8a8408ca66909e922e397c39
zayneshzangar <zangarzaynesh@gmail.com>
2025-02-17 19:41:44 +0500

    change scripts

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./env.sh
-./script-psql-run.sh
-sleep 2
-./script-create-db.sh
--- scripts/env.sh
+++ scripts/env.sh
@@ -4,9 +4,9 @@ export PGPASSWORD='pass123'
 export ROOT_USER_PSQL='postgres'
 export DB_PORT=5432
 export DB_HOST=$(hostname -I | awk '{print $1}')
+export DB_TYPE='postgres'
+export DB_SSLMODE=disable
 
 export USER_PRODUCT_SERVICE='product_service'
 export DB_PRODUCT_SERVICE='product_service'
 export PASSWORD_PRODUCT_SERVICE='nahJ1iyeehei4ori6osh5I'
-export DB_SSLMODE=disable
-export DB_TYPE='postgres'
